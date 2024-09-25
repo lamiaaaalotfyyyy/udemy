7874f383729f81e2fff7b5e5a6cac2026d4f3e00
Mahmoud <mahmoudabdelrahem74@gmail.com>
2024-09-25 14:59:15 +0300

    Update New

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,14 @@ import Top_Categories from "./_components/TopCategories/TopCategories";
 import Topics_Categories from "./_components/TopicsCategories/TopicsCategories";
 import BroadSelection from "./broadSelection/page";
 import LandingPage from "./landingPage/page";
+import LandingPageSection4 from "./landingpageSection/page";
 export default function Home() {
   return (
     <>
       <LandingPage />
-      <BroadSelection />
       <Com_Logos />
       <BroadSelection />
+      <LandingPageSection4/>
       <Top_Categories />
       <Topics_Categories />
       <Become_Instractor />
