c027dab8ae20dc646156b0e7244d7919c01dc465
Mahmoud <mahmoudabdelrahem74@gmail.com>
2024-09-28 18:15:30 +0300

    Add New Three Section

--- next.config.mjs
+++ next.config.mjs
@@ -33,6 +33,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "img-d.udemycdn.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,9 @@ import Topics_Categories from "./_components/TopicsCategories/TopicsCategories";
 import BroadSelection from "./broadSelection/page";
 import LandingPage from "./landingPage/page";
 import LandingPageSection4 from "./landingpageSection/page";
+import LandingPageSection5 from "./landingPageSection5/page";
+import LandingPageSection8 from "./landingPageSection8/page";
+import LandingPageSection9 from "./landingPageSection9/page";
 export default function Home() {
   return (
     <>
@@ -12,7 +15,10 @@ export default function Home() {
       <Com_Logos />
       <BroadSelection />
       <LandingPageSection4/>
+      <LandingPageSection5/>
       <Top_Categories />
+      <LandingPageSection8/>
+      <LandingPageSection9/>
       <Topics_Categories />
       <Become_Instractor />
     </>
